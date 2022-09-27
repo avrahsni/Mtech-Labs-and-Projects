1579c8315f49b22d32097424bc82cccee479494a
Snir Avrahami <sniravrahami@Snirs-MacBook-Pro.local>
2022-09-27 08:40:44 -0600

    Completed the 2.7 Tab Bar Lab

---
+++ student labs/About Me 2.7 Lab Tab Bar Controllers/About Me 2.7 Lab Tab Bar Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me 2.7 Lab Tab Bar Controllers
+//
+//  Created by Snir Avrahami on 9/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
